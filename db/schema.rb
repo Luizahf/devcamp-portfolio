7a666c3eea32efb475d828b17d484bf7a98a47f1
Luizahf <luizahfreire@hotmail.com>
2019-06-17 17:26:29 -0300

    Implemented partials for blog, font awesome, and pagination

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2019_06_14_143732) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "topics"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
